3ac45352a40c35e4e8616ea8192ba47f6c1a98dc
kybermartin <59872693+kybermartin@users.noreply.github.com>
2025-05-15 20:02:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,29 @@ display.set_caption("Ping Pong")
 
 clock = time.Clock()
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, x, y, speed, wight, height, color):
+        self.speed = speed
+        self.color = color
+        self.rect = Rect(x, y, wight, height)
+    
+    def draw(self):
+        draw.rect(screen, self.color, self.rect)
+
+player1 = GameSprite(10, 200, 5, 30, 150, "red")
+player2 = GameSprite(760, 200, 5, 30, 150, "blue")
+
 runing = True
 while runing:
 
     for ev in event.get():
         if ev.type == QUIT:
             runing = False
+
+    player1.draw()
+    player2.draw()
+    
+    display.update()
     
     clock.tick(FPS)
         
