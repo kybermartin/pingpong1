88b1b8615a38473c900971dbc53fa970e311b86b
kybermartin <59872693+kybermartin@users.noreply.github.com>
2025-05-15 19:27:22 +0200

    pridal som súbor hry
    aplikoval som šablónu hry

---
+++ main.py
@@ -0,0 +1,21 @@
+from pygame import *
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+display.set_caption("Ping Pong")
+
+clock = time.Clock()
+
+runing = True
+while runing:
+
+    for ev in event.get():
+        if ev.type == QUIT:
+            runing = False
+    
+    clock.tick(FPS)
+        
+quit()
